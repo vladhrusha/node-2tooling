27123580ac9b52e08a083794795a87d99e53763c
Vlad Hrusha <hrushavladyslavwork@gmail.com>
2023-04-09 14:24:25 +0300

    commit 8

--- 2.1_codeStyle/index.js
+++ 2.1_codeStyle/index.js
@@ -1,3 +1,8 @@
+/* eslint-disable */
 console.log("test");
 console.log("test change");
 console.log("test change 2");
+console.log("test change 3");
+console.log("test change 4");
+
+/* eslint-enable */

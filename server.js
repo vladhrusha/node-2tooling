c0ec09ba69ebd0ca5e2c6cf34ba0fecbae16abee
Vlad Hrusha <hrushavladyslavwork@gmail.com>
2023-04-21 16:58:53 +0300

    translated requests to async

--- server.js
+++ server.js
@@ -4,42 +4,38 @@ const app = express();
 require("dotenv").config();
 
 const port = process.env.PORT || 3002;
-app.get("/random", (req, res) => {
-  axios
-    .get("https://yesno.wtf/api")
-    .then((response) => {
-      switch (response.data.answer) {
-        case "yes":
-          res.send({ result: "true" });
-          break;
-        case "no":
-          res.send({ result: "false" });
-          break;
-        case "maybe":
-          res.sendStatus(500);
-          break;
-        default:
-          break;
-      }
-    })
-    .catch((error) => {
-      // eslint-disable-next-line
-      console.error(error);
-      res.status(500).send("Error fetching data from API");
-    });
+app.get("/random", async (req, res) => {
+  try {
+    const response = await axios.get("https://yesno.wtf/api");
+    switch (response.data.answer) {
+      case "yes":
+        res.send({ result: "true" });
+        break;
+      case "no":
+        res.send({ result: "false" });
+        break;
+      case "maybe":
+        res.sendStatus(500);
+        break;
+      default:
+        break;
+    }
+  } catch (error) {
+    // eslint-disable-next-line
+    console.error(error);
+    res.status(500).send("Error fetching data from API");
+  }
 });
 
-app.get("/healthz", (req, res) => {
-  axios
-    .head("https://yesno.wtf")
-    .then((response) => {
-      res.sendStatus(200);
-    })
-    .catch((error) => {
-      // eslint-disable-next-line
-      console.error(error);
-      res.sendStatus(500);
-    });
+app.get("/healthz", async (req, res) => {
+  try {
+    await axios.head("https://yesno.wtf");
+    res.sendStatus(200);
+  } catch (error) {
+    // eslint-disable-next-line
+    console.log(error);
+    res.sendStatus(500);
+  }
 });
 
 // eslint-disable-next-line
